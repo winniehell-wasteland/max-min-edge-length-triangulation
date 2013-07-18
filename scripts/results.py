f6c0dd8a54986429198c7e0bc5e1c2624b344c0c
winniehell <git@winniehell.de>
2013-07-18 21:10:53 +0200

    scripts

---
+++ scripts/results.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python2
+
+import json
+import pandas
+import matplotlib.pyplot as plt
+from matplotlib import rc as mpl_rc
+import numpy as np
+
+
+FIG_SIZE = (20, 10)
+X_TICK_STEP = 10
+
+# use LaTeX for matplotlib
+mpl_rc('text', usetex=True)
+mpl_rc('text.latex',
+       preamble=r'\usepackage{amsmath}'
+                '\usepackage{fourier}'
+                '\usepackage{xfrac}'
+       )
+
+
+# error function for a given deviation (e.g. std or mad)
+def err(dev):
+    c = 2.0
+
+    def func(a):
+        return c * dev(a, ddof=1) / np.sqrt(len(a))
+    func.__name__ = 'err'
+
+    return func
+
+
+# remove hierarchical structures from object
+def flatten(obj, prefix=''):
+    if isinstance(obj, dict):
+        flattened_obj = {}
+
+        for key, value in obj.iteritems():
+            if isinstance(value, dict):
+                flattened_obj.update(flatten(value, prefix + key + '/'))
+            else:
+                flattened_obj[prefix + key] = value
+
+        return flattened_obj
+    elif isinstance(obj, list):
+        return list(
+            flatten(value)
+            for value
+            in obj
+        )
+    else:
+        return obj
+
+
+# load results file into pandas.DataFrame
+def load(*columns):
+    with open('results.json', 'r') as results_file:
+        results = json.load(results_file)
+
+    results = flatten(results)
+    results = pandas.concat([pandas.DataFrame(results[i], index=[i]) for i in range(len(results))])
+
+    if len(columns) > 0:
+        results = results[[column for column in columns]]
+        return results
+
+
+# median absolute deviation (ddof ignored for now)
+def mad(a, ddof):
+    return np.median([abs(val - np.median(a)) for val in a])
+
+
+# transform line segment lengths to unit square
+def normalize(data, values):
+    for value in values:
+        data[value.replace('squared_length', 'length')] = np.sqrt(1.0 * data[value]) / data['file/range']
+
+
+def output(name, data, columns, aggregations, instances):
+    """
+    store matplotlib graph as pdf and table as html
+    """
+    data.columns = pandas.MultiIndex.from_tuples([
+        (column, agg.__name__)
+        for column
+        in columns.itervalues()
+        for agg
+        in aggregations
+    ])
+
+    if instances is not None:
+        data[('instances', ' ')] = [instances[i] for i in data.index]
+
+    #plt.show()
+    plt.savefig(name + '.pdf', bbox_inches='tight')
+
+    with open(name + '.html', 'w') as html_file:
+        html_file.write(data.to_html())
+
+
+def plot_axes(data, axes, log_scale, x_min=None, y_min=None):
+    """
+    :type axes: matplotlib.pyplot.Axes
+    :type data: pandas.DataFrame
+    :type log_scale: bool
+    """
+
+    # position legend
+    legend = axes.legend(loc='best', prop={'size': 10})
+    plt.setp(legend.get_texts(), fontsize='large')
+
+    #x_range = axes.get_xlim()
+
+    if x_min is None:
+        x_min = X_TICK_STEP / 2
+
+    x_range = (x_min, max(data.index) + X_TICK_STEP / 2)
+    x_ticks = np.arange(X_TICK_STEP * np.ceil(x_range[0] / X_TICK_STEP), x_range[1] + 1, X_TICK_STEP)
+
+    y_range = axes.get_ylim()
+
+    if log_scale:
+        axes.set_yscale('log')
+
+    if y_min is None:
+        y_min = int(log_scale)
+
+    if log_scale:
+        y_range = (10 ** int(np.log10(y_min)), 10 ** int(np.log10(y_range[1]) + 1))
+        y_ticks = [10 ** i for i in np.arange(np.log10(y_range[0]), np.log10(y_range[1]) + 1)]
+    else:
+        y_range = (y_min, int(np.ceil(y_range[1] + 1)))
+        y_ticks = np.arange(max(0, y_range[0]), y_range[1] + 1, max(1, (y_range[1] - y_range[0]) / FIG_SIZE[1]))
+
+    axes.set_ylim(y_range)
+    axes.set_yticks(y_ticks)
+
+    # mirror y-axes
+    ax2 = axes.twinx()
+
+    if log_scale:
+        ax2.set_yscale('log')
+
+    ax2.set_xlim(x_range)
+    ax2.set_xticks(x_ticks)
+    ax2.set_ylim(y_range)
+    ax2.set_yticks(y_ticks)
+
+
+def plot_error(data, columns, aggregations):
+    """
+    plot columns of data with error bars
+    assumes aggregations = [val, err]
+    :rtype : matplotlib.pyplot.Axes
+    """
+
+    fig = plt.figure(figsize=FIG_SIZE)
+    axes = fig.add_subplot(111)
+
+    for key, label in columns.iteritems():
+        column = data[key]
+        min_err = np.minimum(column[aggregations[1].__name__], column[aggregations[0].__name__] - 1)
+        axes.errorbar(
+            column.index, column[aggregations[0].__name__],
+            yerr=[min_err, column[aggregations[1].__name__]],
+            label=label,
+            marker='o',
+            linestyle=' '
+        )
+
+    return axes
---
+++ scripts/times.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python2
+
+import math
+import numpy as np
+
+import pandas as pd
+import results
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+DEBUG = False
+#DEBUG = True
+FIG_SIZE = (20, 10)
+X_TICK_STEP = 10
+
+if DEBUG:
+    data = 1 / 0
+else:
+    print 'Loading results...'
+    data = results.load(
+        'file/num_points',
+        'file/iteration',
+        'file/range',
+        'time/total',
+        'time/SAT_solving',
+        'time/generating_segments',
+        'shortest/non-crossing/index'
+    )
+
+
+def output_time_comparison(data, instances):
+    print 'Generating time comparison...'
+
+    columns = {
+        'time/total': 'total running time',
+        'time/SAT_solving': 'time for SAT solving',
+        'time/generating_segments': 'time for segment generation'
+    }
+    aggregations = [
+        np.median,
+        results.err(results.mad)
+    ]
+
+    data = data[columns.keys()]
+    data = data.agg(aggregations)
+
+    data.index.name = 'number of points'
+
+    #print data
+
+    axes = results.plot_error(data=data, columns=columns, aggregations=aggregations)
+
+    axes.set_xlabel(data.index.name)
+    axes.set_ylabel('time in milliseconds')
+    results.plot_axes(data=data, axes=axes, log_scale=True, y_min=(10 ** -1))
+
+    results.output('time_comparison', data=data, columns=columns, aggregations=aggregations, instances=instances)
+
+
+def output_time_total(data, instances):
+    print 'Generating total time...'
+
+    columns = {
+        'time/total': 'total running time'
+    }
+    aggregations = [
+        np.median,
+        results.err(results.mad)
+    ]
+
+    data = data[columns.keys()]
+    data = data.agg(aggregations)
+
+    data.index.name = 'number of points'
+
+    #print data
+
+    axes = results.plot_error(data=data, columns=columns, aggregations=aggregations)
+
+    def func(n, a, b, c):
+        return a * np.power(n, b) + c
+
+    params = curve_fit(func, data.index.values, data['time/total']['median'])[0]
+
+    plt.plot(
+        data.index, func(data.index, params[0], params[1], params[2]),
+        label=r'\( ' + str(params[0]) + ' \cdot n^{' + str(params[1]) + '} + ' + str(params[2]) + '\)'
+    )
+
+    axes.set_xlabel(data.index.name)
+    axes.set_ylabel('time in milliseconds')
+    results.plot_axes(data=data, axes=axes, log_scale=True)
+
+    results.output('time_total', data=data, columns=columns, aggregations=aggregations, instances=instances)
+
+
+def output_time_noncrossing(data, instances, threshold):
+    print 'Generating time noncrossing...'
+
+    columns = {
+        'time/total': 'total running time'
+    }
+    aggregations = [
+        np.median,
+        results.err(results.mad)
+    ]
+
+    data = data[columns.keys()]
+    data = data.agg(aggregations)
+    data = data[data.index <= np.min([i for i in data.index if instances[i] < threshold])]
+
+    data.index.name = 'shortest non-crossing segment index'
+
+    #print data
+
+    axes = results.plot_error(data=data, columns=columns, aggregations=aggregations)
+
+    #def func(n, a, b, c):
+    #    return a * np.power(n, b) + c
+
+    #params = curve_fit(func, data.index.values, data['mean'])[0]
+
+    #axes.plot(
+    #    data.index, func(data.index, params[0], params[1], params[2]),
+    #    label=r'\( ' + str(params[0]) + ' \cdot n^{' + str(params[1]) + '} + ' + str(params[2]) + '\)'
+    #)
+
+    axes.set_xlabel(data.index.name)
+    axes.set_ylabel('time in milliseconds')
+    results.plot_axes(data=data, axes=axes, log_scale=True, x_min=-1)
+
+    x_range = axes.get_xlim()
+    axes.set_xticks(np.arange(0, x_range[1], 1))
+
+    results.output('time_noncrossing', data=data, columns=columns, aggregations=aggregations, instances=instances)
+
+
+def output_time_hist(data, threshold):
+    print 'Output time histogram...'
+
+    def count(a):
+        return len(a)
+
+    columns = {
+        'file/iteration': 'instances'
+    }
+    aggregations = [
+        count
+    ]
+
+    data = data[data['time/total'] < threshold]
+    data = data.groupby('file/num_points')
+
+    data = data[columns.keys()]
+    data = data.agg(aggregations)
+
+    #print data
+
+    data.index.name = 'number of points'
+
+    fig = plt.figure(figsize=FIG_SIZE)
+    axes = fig.add_subplot(111)
+
+    for key, label in columns.iteritems():
+        column = data[key]
+        axes.bar(data.index, column['count'], label=label, width=10, align='center')
+
+    axes.set_xlabel(data.index.name)
+    axes.set_ylabel('instances < ' + str(threshold) + ' milliseconds')
+    results.plot_axes(data=data, axes=axes, log_scale=False, y_min=0)
+
+    results.output('time_hist', data=data, columns=columns, aggregations=aggregations, instances=None)
+
+
+def output_time_allhist(data):
+    print 'Output all time histograms...'
+
+    for points in np.unique(data['file/num_points']):
+        hist_data = data[data['file/num_points'] == points]
+
+        fig = plt.figure(figsize=FIG_SIZE)
+
+        ax = fig.add_subplot(111)
+        #ax.set_xlabel('shortest non-crossing segment index')
+        #ax.set_ylabel('time in milliseconds')
+
+        plt.hist(hist_data['time/total'], bins=50)
+
+        results.output('time_hist_' + str(points), hist_data)
+
+x_column = 'file/num_points'
+instances = {
+    x_val: len(data[data[x_column] == x_val])
+    for x_val
+    in data[x_column]
+}
+data = data.groupby(x_column)
+output_time_comparison(data, instances)
+output_time_total(data, instances)
+data = data.obj
+
+x_column = 'shortest/non-crossing/index'
+instances = {
+    x_val: len(data[data[x_column] == x_val])
+    for x_val
+    in data[x_column]
+}
+data = data.groupby(x_column)
+output_time_noncrossing(data, instances, threshold=50)
+data = data.obj
+
+output_time_hist(data, 30000)
+#output_time_allhist(data)
