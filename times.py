4a406784c6f8cb2d6023cc1af578ab612e1a00b8
winniehell <git@winniehell.de>
2013-07-21 01:03:10 +0200

    remove unused DEBUG mode, use format strings, fix histogram, change time offset to 30min

--- times.py
+++ times.py
@@ -6,22 +6,16 @@ import results
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-DEBUG = False
-#DEBUG = True
-
-if DEBUG:
-    data = 1 / 0
-else:
-    print 'Loading results...'
-    data = results.load(
-        'file/num_points',
-        'file/iteration',
-        'file/range',
-        'time/total',
-        'time/SAT_solving',
-        'time/generating_segments',
-        'shortest/non-crossing/index'
-    )
+print 'Loading results...'
+data = results.load(
+    'file/num_points',
+    'file/iteration',
+    'file/range',
+    'time/total',
+    'time/SAT_solving',
+    'time/generating_segments',
+    'shortest/non-crossing/index'
+)
 
 
 def output_time_comparison(data, instances):
@@ -44,13 +38,23 @@ def output_time_comparison(data, instances):
 
     #print data
 
-    axes = results.plot_error(data=data, columns=columns, aggregations=aggregations)
+    axes = results.plot_error(
+        data=data,
+        columns=columns,
+        aggregations=aggregations
+    )
 
     axes.set_xlabel(data.index.name)
     axes.set_ylabel('time in milliseconds')
     results.plot_axes(data=data, axes=axes, log_scale=True, y_min=(10 ** -1))
 
-    results.output('time_comparison', data=data, columns=columns, aggregations=aggregations, instances=instances)
+    results.output(
+        'time_comparison',
+        data=data,
+        columns=columns,
+        aggregations=aggregations,
+        instances=instances
+    )
 
 
 def output_time_total(data, instances):
@@ -71,7 +75,11 @@ def output_time_total(data, instances):
 
     #print data
 
-    axes = results.plot_error(data=data, columns=columns, aggregations=aggregations)
+    axes = results.plot_error(
+        data=data,
+        columns=columns,
+        aggregations=aggregations
+    )
 
     def func(n, a, b, c):
         return a * np.power(n, b) + c
@@ -80,14 +88,20 @@ def output_time_total(data, instances):
 
     plt.plot(
         data.index, func(data.index, params[0], params[1], params[2]),
-        label=r'\( ' + str(params[0]) + ' \cdot n^{' + str(params[1]) + '} + ' + str(params[2]) + '\)'
+        label=r'\({0:.2f} \cdot n^{{1:.2f}} {2:+.2f}\)'.format(params)
     )
 
     axes.set_xlabel(data.index.name)
     axes.set_ylabel('time in milliseconds')
     results.plot_axes(data=data, axes=axes, log_scale=True)
 
-    results.output('time_total', data=data, columns=columns, aggregations=aggregations, instances=instances)
+    results.output(
+        'time_total',
+        data=data,
+        columns=columns,
+        aggregations=aggregations,
+        instances=instances
+    )
 
 
 def output_time_noncrossing(data, instances, threshold):
@@ -103,23 +117,19 @@ def output_time_noncrossing(data, instances, threshold):
 
     data = data[columns.keys()]
     data = data.agg(aggregations)
-    data = data[data.index <= np.min([i for i in data.index if instances[i] < threshold])]
+    data = data[data.index <= np.min(
+        [i for i in data.index if instances[i] < threshold]
+    )]
 
     data.index.name = 'shortest non-crossing segment index'
 
     #print data
 
-    axes = results.plot_error(data=data, columns=columns, aggregations=aggregations)
-
-    #def func(n, a, b, c):
-    #    return a * np.power(n, b) + c
-
-    #params = curve_fit(func, data.index.values, data['median'])[0]
-
-    #axes.plot(
-    #    data.index, func(data.index, params[0], params[1], params[2]),
-    #    label=r'\( ' + str(params[0]) + ' \cdot n^{' + str(params[1]) + '} + ' + str(params[2]) + '\)'
-    #)
+    axes = results.plot_error(
+        data=data,
+        columns=columns,
+        aggregations=aggregations
+    )
 
     axes.set_xlabel(data.index.name)
     axes.set_ylabel('time in milliseconds')
@@ -128,7 +138,13 @@ def output_time_noncrossing(data, instances, threshold):
     x_range = axes.get_xlim()
     axes.set_xticks(np.arange(0, x_range[1], 1))
 
-    results.output('time_noncrossing', data=data, columns=columns, aggregations=aggregations, instances=instances)
+    results.output(
+        'time_noncrossing',
+        data=data,
+        columns=columns,
+        aggregations=aggregations,
+        instances=instances
+    )
 
 
 def output_time_hist(data, threshold):
@@ -159,13 +175,25 @@ def output_time_hist(data, threshold):
 
     for key, label in columns.iteritems():
         column = data[key]
-        axes.bar(data.index, column['count'], label=label, width=10, align='center')
+        axes.bar(
+            data.index,
+            column['count'],
+            label=label,
+            width=10,
+            align='center'
+        )
 
     axes.set_xlabel(data.index.name)
     axes.set_ylabel('instances < ' + str(threshold) + ' milliseconds')
     results.plot_axes(data=data, axes=axes, log_scale=False, y_min=0)
 
-    results.output('time_hist', data=data, columns=columns, aggregations=aggregations, instances=None)
+    results.output(
+        'time_hist',
+        data=data,
+        columns=columns,
+        aggregations=aggregations,
+        instances=None
+    )
 
 
 def output_time_allhist(data):
@@ -175,15 +203,14 @@ def output_time_allhist(data):
         hist_data = data[data['file/num_points'] == points]
 
         fig = plt.figure(figsize=results.FIG_SIZE)
+        axes = fig.add_subplot(111)
 
-        ax = fig.add_subplot(111)
-        #ax.set_xlabel('shortest non-crossing segment index')
-        #ax.set_ylabel('time in milliseconds')
+        axes.set_xlabel('time in milliseconds')
+        axes.set_ylabel('number of instances')
 
-        plt.hist(hist_data['time/total'], bins=50)
+        axes.hist(hist_data['time/total'], bins=50)
 
-        # TODO
-        #results.output('time_hist_' + str(points), hist_data)
+        axes.savefig('time_hist_' + str(points) + '.pdf', bbox_inches='tight')
 
 if __name__ == '__main__':
     x_column = 'file/num_points'
@@ -207,5 +234,5 @@ if __name__ == '__main__':
     output_time_noncrossing(data, instances, threshold=50)
     data = data.obj
 
-    output_time_hist(data, 30000)
+    output_time_hist(data, 30 * 60 * 1000)
     #output_time_allhist(data)
